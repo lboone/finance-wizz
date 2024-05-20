9a7df97e793b02a699751f1abb476814028e6e4d
Lloyd Boone <lboone@progressive.com>
2024-05-20 15:01:49 -0400

    implement hono for handling api routes

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server';
 const isNotProtected = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/api(.*)',
 ]);
 
 export default clerkMiddleware((auth, req) => {
